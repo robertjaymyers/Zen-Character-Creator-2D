c533eb1fdd6909108d079e700f918f95ee4d3a19
Robert Jay Myers <myers.robertj@gmail.com>
2019-12-08 17:05:18 -0500

    Disabled maximize button to make window size more strictly fixed.

--- CharacterCreator2d/main.cpp
+++ CharacterCreator2d/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	CharacterCreator2d w;
+	w.setWindowFlags(Qt::Window | Qt::WindowCloseButtonHint | Qt::WindowMinimizeButtonHint);
 	w.show();
 	return a.exec();
 }
