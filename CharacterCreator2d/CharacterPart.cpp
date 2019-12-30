9b22d9d72bcbc6bd83ddf85e6607760b2a84df62
Robert Jay Myers <myers.robertj@gmail.com>
2019-12-29 22:58:27 -0500

    Added license.

--- CharacterCreator2d/CharacterPart.cpp
+++ CharacterCreator2d/CharacterPart.cpp
@@ -1,3 +1,17 @@
+/*
+This file is part of CharacterCreator2d.
+	CharacterCreator2d is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	CharacterCreator2d is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with CharacterCreator2d.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "CharacterPart.h"
 
 CharacterPart::CharacterPart(QGraphicsItem* parent, const PartType partType, const QColor color, const ColorSetType colorSetType)
